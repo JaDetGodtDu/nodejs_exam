4fd5c4a3cf05c123c1a8add7cb7e8a0d9cff42bd
JaDetGodtDu <123992392+JaDetGodtDu@users.noreply.github.com>
2025-05-15 10:46:06 +0200

    Profilepage: Read functionality working. TODO: Update & Delete

--- client/src/pages/Profilepage/Profilepage.svelte
+++ client/src/pages/Profilepage/Profilepage.svelte
@@ -1,7 +1,22 @@
 <script>
     import '../page.css';
+    import {onMount} from 'svelte';
+    import {session} from '../../stores/sessionStore';
+
+    let username;
+    let email;
+
+    session.subscribe(value => {
+        username = value.username;
+        email = value.email;
+    })
 </script>
 
 <div class='page'>
     <h1>Profilepage</h1>
+    <div class='profile-container'>
+        <p><b>Username:</b> {username}</p>
+        <p><b>Email:</b> {email}</p>
+
+    </div>
 </div>
